7313a58c29e47bed9786c6968e23256826cd0b0a
Evgenii Antonovich <e.antonovich@otclick.ru>
2024-10-30 07:46:26 +0000

    create match if id == nil, fix err

--- internal/service/entities/match.go
+++ internal/service/entities/match.go
@@ -17,23 +17,23 @@ type Match struct {
 }
 
 type GamesMatch struct {
-	Date           time.Time `json:"date" validate:"required,valid-date"`
-	Team1ID        int       `json:"team1Id" validate:"required,gt=0"`
-	Team2ID        int       `json:"team2Id" validate:"required,gt=0"`
-	Player1Team1Id int       `json:"player1Team1Id" validate:"required,gt=0"`
-	Player2Team1Id *int      `json:"player2Team1Id"`
-	Player1Team2Id int       `json:"player1Team2Id" validate:"required,gt=0"`
-	Player2Team2Id *int      `json:"player2Team2Id"`
-	ScoreTeam1     int       `json:"scoreTeam1" validate:"required,gte=0"`
-	ScoreTeam2     int       `json:"scoreTeam2" validate:"required,gte=0"`
-	Player1Team1RateBefore *int    `db:"player1_team1_rate_before" json:"player1Team1IdRateBefore"`
-	Player2Team1RateBefore *int    `db:"player2_team1_rate_before" json:"player2Team1IdRateBefore"`
-	Player1Team2RateBefore *int    `db:"player1_team2_rate_before" json:"player1Team2IdRateBefore"`
-	Player2Team2RateBefore *int    `db:"player2_team2_rate_before" json:"player2Team2IdRateBefore"`
-	Player1Team1RateAfter *int    `db:"player1_team1_rate_after" json:"player1Team1IdRateAfter"`
-	Player2Team1RateAfter *int    `db:"player2_team1_rate_after" json:"player2Team1IdRateAfter"`
-	Player1Team2RateAfter *int    `db:"player1_team2_rate_after" json:"player1Team2IdRateAfter"`
-	Player2Team2RateAfter *int    `db:"player2_team2_rate_after" json:"player2Team2IdRateAfter"`
+	Date                   time.Time `json:"date" validate:"required,valid-date"`
+	Team1ID                int       `json:"team1Id" validate:"required,gt=0"`
+	Team2ID                int       `json:"team2Id" validate:"required,gt=0"`
+	Player1Team1Id         int       `json:"player1Team1Id" validate:"required,gt=0"`
+	Player2Team1Id         *int      `json:"player2Team1Id"`
+	Player1Team2Id         int       `json:"player1Team2Id" validate:"required,gt=0"`
+	Player2Team2Id         *int      `json:"player2Team2Id"`
+	ScoreTeam1             int       `json:"scoreTeam1" validate:"required,gte=0"`
+	ScoreTeam2             int       `json:"scoreTeam2" validate:"required,gte=0"`
+	Player1Team1RateBefore *int      `db:"player1_team1_rate_before" json:"player1Team1IdRateBefore"`
+	Player2Team1RateBefore *int      `db:"player2_team1_rate_before" json:"player2Team1IdRateBefore"`
+	Player1Team2RateBefore *int      `db:"player1_team2_rate_before" json:"player1Team2IdRateBefore"`
+	Player2Team2RateBefore *int      `db:"player2_team2_rate_before" json:"player2Team2IdRateBefore"`
+	Player1Team1RateAfter  *int      `db:"player1_team1_rate_after" json:"player1Team1IdRateAfter"`
+	Player2Team1RateAfter  *int      `db:"player2_team1_rate_after" json:"player2Team1IdRateAfter"`
+	Player1Team2RateAfter  *int      `db:"player1_team2_rate_after" json:"player1Team2IdRateAfter"`
+	Player2Team2RateAfter  *int      `db:"player2_team2_rate_after" json:"player2Team2IdRateAfter"`
 }
 
 type FullMatch struct {
@@ -54,7 +54,7 @@ type FullMatch struct {
 }
 
 type NewMatch struct {
-	ID             int       `json:"id" validate:"gt=0"`
+	ID             *int      `json:"id"`
 	Date           time.Time `json:"date" validate:"required,valid-date"`
 	Team1ID        int       `json:"team1Id" validate:"gt=0"`
 	Team2ID        int       `json:"team2Id" validate:"gt=0"`
